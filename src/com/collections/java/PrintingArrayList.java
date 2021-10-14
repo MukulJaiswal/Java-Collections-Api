4eaeb445a00db20299400de20dc09fced9341a42
Mukul <mukul.jaiswal786@gmail.com>
2021-10-15 00:50:51 +0530

    ConcurrentModificationException corrected..

--- src/com/collections/java/PrintingArrayList.java
+++ src/com/collections/java/PrintingArrayList.java
@@ -22,20 +22,38 @@ public class PrintingArrayList {
 		values.add(100);
 		values.add(200);
 		values.add(300);
+		values.add(400);
+		values.add(500);
 
-		// Enhanced For Loop
-		for (Integer i : values) {
+		/**
+		 * In Enhanced-for loop, adding and removing elements will give
+		 * ConcurrentModificationException always
+		 */
+		for (int i : values) {
 //			values.add(1000); //This will give concurrent modification exception
-//			values.remove(100); //ArrayIndexOutOfBound
-			System.out.println(i);
+			
+			if(i == 200) {
+				//This will remove element by index
+				//values.remove(1); //ConcurrentModificationException
+				//This will remove element by values
+				//values.remove(new Integer(200)); ////ConcurrentModificationException
+			}
 		}
+		
+		System.out.println("\nEnhanced-for loop : " + values);
 		
 		for (int i = 0; i < values.size(); i++) {
-//			values.remove(100);////ArrayIndexOutOfBound
+			
+			if(values.get(i) == 200) {
+//				values.remove(1); //This will work but should not be done as it changed the index of list
+			}
+
 //			values.add(300);//Infinite loop
-			System.out.println(values.get(i));
+			
+			System.out.println(values);
 		}
 		
+		System.out.println("\nNormal-for loop : " + values);
 		//*************************************Iterator*****************************
 		
 		//Difference between Iterator and ListIterator
@@ -51,7 +69,7 @@ public class PrintingArrayList {
 			
 			if(val == 200) {
 				iter.remove();
-				System.out.println(val  + "is removed");
+				System.out.println("\n" + val  + "is removed");
 			}
 		}
 		
